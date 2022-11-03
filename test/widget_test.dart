0b01a28d4a55cea75c7f66416f0976f669242a9b
ValiiJ <vali.jollxhiu25@gmail.com>
2022-11-03 17:57:02 +0100

    A  magic ball page was created

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:magic_8_ball_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Ball());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
